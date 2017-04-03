bcc2230152aed00ade64356b8cc1808a0c5f4c67
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-04-03 15:15:11 -0700

    Improve test

--- test/test.ml
+++ test/test.ml
@@ -1,6 +1,6 @@
 let comma =
   let open Farfadet in
-  (fun e () -> eval e [ string $ "," ]), ()
+  (fun e () -> string e ","), ()
 
 let memcpy s soff d doff len =
   for i = 0 to len - 1
@@ -9,35 +9,53 @@ let memcpy s soff d doff len =
 let rec value : Ezjsonm.value Farfadet.t = fun e x ->
   let open Farfadet in
 
-  let binding e (k, v) = eval e [ !!string; char $ ':'; !!value ] k v in
+  let binding e (k, v) = eval e [ char $ '"'; !!string; char $ '"'; char $ ':'; !!value ] k v in
   let arr = list ~sep:comma value in
   let obj = list ~sep:comma binding in
 
   match x with
-  | `Bool true  -> eval e [ string $ "true" ]
-  | `Bool false -> eval e [ string $ "false" ]
-  | `Float f    -> eval e [ string $ (Format.sprintf "%.16g" f) ]
-  | `Null       -> eval e [ string $ "null" ]
-  | `String s   -> eval e [ char $ '"'; !!(whole @@ blitter String.length memcpy); char $ '"' ] s (* for fun *)
+  | `Bool true  -> string e "true"
+  | `Bool false -> string e "false"
+  | `Float f    -> string e (Format.sprintf "%.16g" f)
+  | `Null       -> string e "null"
+  | `String s   -> eval e [ char $ '"'; !!(whole @@ blitter String.length memcpy); char $ '"' ] s (* just for fun *)
   | `A a        -> eval e [ char $ '['; !!arr; char $ ']' ] a
   | `O o        -> eval e [ char $ '{'; !!obj; char $ '}' ] o
 
 let json e x =
   let open Farfadet in
 
-  let binding e (k, v) = eval e [ !!string; char $ ':'; !!value ] k v in
+  let binding e (k, v) = eval e [ char $ '"'; !!string; char $ '"'; char $ ':'; !!value ] k v in
   let arr = list ~sep:comma value in
   let obj = list ~sep:comma binding in
 
   match x with
   | `A a -> eval e [ char $ '['; !!arr; char $ ']' ] a
   | `O o -> eval e [ char $ '{'; !!obj; char $ '}' ] o
-  (* mais c'est trop beau ! *)
 
-let test1 = `A [ `Bool true ]
+let tests =
+  [ `A [ `Bool true ]
+  ; `O [ ("a", `A [ `Bool true; `Bool false]) ]
+  ; `A [ `O [ ("a", `Bool true); ("b", `Bool false) ] ] ]
+
+let ezjsonm =
+  Alcotest.testable
+    (fun fmt value -> Format.fprintf fmt "%s" (Ezjsonm.to_string value))
+    (=)
+
+let make_test value =
+  Format.sprintf "%s" (Ezjsonm.to_string value),
+  `Slow,
+  (fun () ->
+     let enc = Faraday.create 0x800 in
+
+     Farfadet.(eval enc [ !!json ] value);
+
+     let str = Faraday.serialize_to_string enc in
+     let res = Ezjsonm.from_string str in
+
+     Alcotest.(check ezjsonm) str value res)
 
 let () =
-  let e = Faraday.create 0x800 in
-
-  Farfadet.(eval e [ !!json ] test1);
-  Format.printf "%s\n%!" (Faraday.serialize_to_string e)
+  Alcotest.run "farfadet test"
+    [ "simple", List.map make_test tests ]
